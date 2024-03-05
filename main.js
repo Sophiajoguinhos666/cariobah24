8cd31f86f621b0517078831792dfe32a3045b44b
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-03-05 09:09:24 -0300

    g

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ alert('Ola mundo');
 let nomeusuario = "";
 let anterior = document.querySelector('#texto');
 
-while(nomeusuario == "" || nomeusuario == null){
+while(nomeusuario == ""){
     nomeusuario = prompt('Qual o seu nome?');
+}
+
+if(nomeusuario == null){
+    anterior = document.querySelector('#texto');
+} else{nterior.textContent = nomeusuario;
+
 }
 
 anterior.textContent = nomeusuario;
