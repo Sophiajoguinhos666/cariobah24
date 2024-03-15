763ce1247cb9ef5212808aa85dae86fddb7553e9
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-03-15 12:00:15 -0300

    m

--- joguinhos.js
+++ joguinhos.js
@@ -1,16 +1,16 @@
-let numeroSecreto = Math.floor(Math.random()*69);
-console.log(numeroSecreto) ;
+let numeroSecreto = Math.floor(Math.random() * 69);
+console.log(numeroSecreto);
 let chute = '';
 
 
-while(chute == ''){
-    chute = prompt ('escolha um número de 0 a 100');
+while (chute == '') {
+    chute = prompt('escolha um número de 0 a 100');
 }
 
-if(numeroSecreto == chute){
-    alert( 'boa chefia' );
+if (numeroSecreto == chute) {
+    alert('boa chefia');
     console.log('parabens, acertou !!!');
-}else{
- alert('errou, acéfalo');
- console.log(' seu infeliz');
+} else {
+    alert('errou, acéfalo');
+    console.log(' seu infeliz');
 }
