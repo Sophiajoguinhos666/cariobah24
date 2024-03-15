337aacdc8533a94476eef6b3d4339f9f4b9c9a03
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-03-15 11:36:38 -0300

    u

---
+++ joguinhos.js
@@ -0,0 +1,12 @@
+let numeroSecreto = 69;
+
+let chute = prompt ('escolha um número de 0 a 100');
+
+if(numeroSecreto == chute){
+    alert( 'boa chefia' );
+    console.log('parabens, acertou !!!');
+}else{
+ alert('errou, acéfalo');
+ console.log(' seu infeliz');
+
+}
