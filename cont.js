c1b289831c975a4e137aaaf76d69a0b25f26db9f
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-05 12:21:18 -0300

    ii

--- cont.js
+++ cont.js
@@ -4,7 +4,9 @@ const botoes = document. querySelectorAll('.objetivos');
 for (let x  =0; x<botoes.length; x++){
     console.log(x)
     botoes[x].onclick = function () {
-     botoes[x].classList.add('ativo')
+        for(let y = 0; y< botoes.length; ; y++){
+            botoes[y].classList.remove('ativo');
+            botoes[x].classList.add('ativo') 
 
      // botoes[x].classList.remove('ativo');
     }
