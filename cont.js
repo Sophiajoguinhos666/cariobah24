95bfef906edac4e814a8449e8ee2d44eaac2bef8
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-05 12:13:10 -0300

    v

--- cont.js
+++ cont.js
@@ -3,6 +3,7 @@ const botoes = document. querySelectorAll('.objetivos');
 
 for (let x  =0; x<botoes.length; x++){
     console.log(x)
+    botoes[x].onclick = botoes[x].classList.add('ativo')
 
 
 }
