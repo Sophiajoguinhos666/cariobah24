14d8df8147e8eec88e4306f3cfd13c0b38aba58a
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-26 09:25:52 -0300

    asda

--- cont.js
+++ cont.js
@@ -15,31 +15,31 @@ for (let x = 0; x < botoes.length; x++) {
     }
 }
 
-const contadores = document.querySelectorAll('.contador');
-let tempoAtual = new Date();
-let tempoObjetivo1 = new Date('2025-10-29T00:00:00');
-let tempoObjetivo2 = new Date('2007-03-20T00:00:00');
-let tempoObjetivo3 = new Date('2024-11-13T00:00:00');
-let tempoObjetivo4 = new Date('2025-03-24T00:00:00');
+    const contadores = document.querySelectorAll('.contador');
+    let tempoAtual = new Date();
+    let tempoObjetivo1 = new Date('2025-10-29T00:00:00');
+    let tempoObjetivo2 = new Date('2007-03-20T00:00:00');
+    let tempoObjetivo3 = new Date('2024-11-13T00:00:00');
+    let tempoObjetivo4 = new Date('2025-03-24T00:00:00');
 
-let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+    let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
 
-function calculaTempo(tempoObjetivo) {
+    function calculaTempo(tempoObjetivo) {
 
-    let TempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(TempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24)
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
+        let TempoFinal = tempoObjetivo - tempoAtual;
+        let segundos = Math.floor(TempoFinal / 1000);
+        let minutos = Math.floor(segundos / 60);
+        let horas = Math.floor(minutos / 60);
+        let dias = Math.floor(horas / 24)
+        segundos %= 60;
+        minutos %= 60;
+        horas %= 24;
 
 
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
-}
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    }
