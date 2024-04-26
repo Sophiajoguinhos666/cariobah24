7e17911d7c8a0f882db875cf078cf95bec4e6f31
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-26 08:45:10 -0300

    dsada

--- cont.js
+++ cont.js
@@ -19,7 +19,7 @@ const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date ();
 let tempoObjetivo1 = new Date('2024-04-29T00:00:00');
 let TempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = TempoFinal/1000;
+let segundos = Math.floor (TempoFinal/1000);
 
 
 
