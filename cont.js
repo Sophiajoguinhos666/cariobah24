543ad483026d17c4dcabc89edfa9984ef0d5b0d3
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-26 09:14:20 -0300

    a

--- cont.js
+++ cont.js
@@ -21,19 +21,25 @@ let tempoObjetivo1 = new Date('2025-10-29T00:00:00');
 let tempoObjetivo2 = new Date('2024-04-25T00:00:00');
 let tempoObjetivo3 = new Date('2024-11-13T00:00:00');
 let tempoObjetivo4 = new Date('2025-03-24T00:00:00');
+
 let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
+
+function calculaTempo(tempoObjetivo) {
+
+    let TempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(TempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24)
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 
 
-let TempoFinal = tempoObjetivo1 - tempoAtual;
-let segundos = Math.floor(TempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24)
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
 
-
-
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}
