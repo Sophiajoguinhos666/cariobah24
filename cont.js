2fe4dbfddd5c510ba1b4baf1df2ff147aa030dc1
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-26 08:55:53 -0300

    saddas

--- cont.js
+++ cont.js
@@ -23,5 +23,10 @@ let segundos = Math.floor (TempoFinal/1000);
 let minutos = Math.floor (segundos/60);
 let horas = Math.floor (minutos/60);    
 let dias = Math.floor (horas/24)
+segundos %= 60;
+minutos %= 60;
+horas %=24;
+
+
 
 contadores [0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos " ;
