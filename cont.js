073d3675feac4a76d946f72994210463670ff044
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-04-26 08:48:33 -0300

    aaaaa

--- cont.js
+++ cont.js
@@ -20,7 +20,8 @@ let tempoAtual = new Date ();
 let tempoObjetivo1 = new Date('2024-04-29T00:00:00');
 let TempoFinal = tempoObjetivo1 - tempoAtual;
 let segundos = Math.floor (TempoFinal/1000);
+let minutos = Math.floor (segundos/60);
+let horas = Math.floor (minutos/60);    
+let dias = Math.floor (horas/24)
 
-
-
-contadores [0].textContent = segundos;
+contadores [0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos" ;
